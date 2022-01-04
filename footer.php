63de8312b2fe63d097e5d76decd1c4eac20851ea
Athul Sankar <83171675+athulsankarvp@users.noreply.github.com>
2022-01-04 19:32:18 +0530

    Initial version

---
+++ footer.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+</head>
+<body>
+    <!--Footer-->
+    <footer class="footer">
+    
+            <div id="footer_text">
+                <p>DBMS Mini Project</p>
+            </div>
+    
+    </footer>
+</body>
+</html>
